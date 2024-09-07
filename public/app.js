d0b1ed75a78bca48c7e078b8b8f88300f4b6dc74
rishitha <rishithasiva1321@gmail.com>
2024-09-06 20:51:15 -0500

    sign out

--- public/app.js
+++ public/app.js
@@ -1,8 +1,10 @@
 import { onClickHomeMenu,onClickMenu2Menu } from "./controller/menueventhandlers.js";
 import { signinPageView } from "./view/signin_page.js";
-
+import { attachAuthStateChangeObserver } from "./controller/firebase_auth.js";
 //menu button handler
 document.getElementById('menu-home').onclick = onClickHomeMenu;
 document.getElementById('menu-menu2').onclick = onClickMenu2Menu;
+
+attachAuthStateChangeObserver();
 
 signinPageView();
--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -1,5 +1,6 @@
 import { 
-    getAuth, signInWithEmailAndPassword 
+    getAuth, signInWithEmailAndPassword,
+    onAuthStateChanged, signOut,
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { app } from "./firebase_core.js";
 import { DEV } from "../model/constants.js";
@@ -13,11 +14,29 @@ export async function signinFirebase(e) {
 
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        const user = userCredential.user;
+      //  const user = userCredential.user;
     } catch (error) {
         if (DEV) console.log('signin error:',error);
         const errorCode = error.code;
         const errorMessage = error.message;
         alert('signin error:' + errorCode + ' ' + errorMessage);
     }
+}
+
+export function attachAuthStateChangeObserver(){
+    onAuthStateChanged(auth,authStateChangeListener);
+
+}
+
+function authStateChangeListener(user){
+    if(user){
+        console.log('user: ', user.email);
+    }else{
+        console.log('signed out');
+    }
+}
+
+export async function signOutFirebase() {
+   await signOut(auth);
+
 }
