3ed29096de378e7a1725a74a985cd370e1380bdd
rishitha <rishithasiva1321@gmail.com>
2024-09-06 20:16:04 -0500

    signin

---
+++ public/controller/firebase_auth.js
@@ -0,0 +1,12 @@
+import { 
+    getAuth, signInWithEmailAndPassword 
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
+import { app } from "./firebase_core.js";
+
+const auth = getAuth(app);
+
+export function signinFirebase(e) {
+    e.preventDefault();
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+}
--- public/view/signin_page.js
+++ public/view/signin_page.js
@@ -1,4 +1,5 @@
 import { root } from "./elements.js";
+import { signinFirebase } from "../controller/firebase_auth.js";
 
 export async function signinPageView(){
     const response = await fetch('/view/templates/signin_page_template.html',
@@ -6,7 +7,14 @@ export async function signinPageView(){
     );
 
     const divWrapper = document.createElement('div'); // <div></div>
+    divWrapper.style.width = "400px";
+    divWrapper.classList.add('m-4','p-4');
     divWrapper.innerHTML = await response.text();
+
+    //attach form submit event listener
+
+    const form = divWrapper.getElementsByTagName('form')[0];
+    form.onsubmit = signinFirebase;
 
     root.innerHTML=''; // clear current page rendering
     root.appendChild(divWrapper);
