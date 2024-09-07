f4ff1e2d6cd7032703bc3bae1f5df32c01bc6502
rishitha <rishithasiva1321@gmail.com>
2024-09-06 22:19:10 -0500

    arrows done

--- public/controller/menueventhandlers.js
+++ public/controller/menueventhandlers.js
@@ -1,11 +1,15 @@
 import { homePageView } from "../view/home_page.js";
 import { Menu2PageView } from "../view/menu2_page.js";
 import { signOutFirebase } from "./firebase_auth.js";
+import { routePathnames } from "./route_controller.js";
 
 export function onClickHomeMenu(e){
+    history.pushState(null, null, routePathnames.HOME);
     homePageView();
 }
 export function onClickMenu2Menu(e){
+    
+    history.pushState(null, null, routePathnames.MENU2);
     Menu2PageView();
 }
 
