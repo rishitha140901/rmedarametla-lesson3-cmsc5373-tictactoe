b22de76078cd702a2f01359682ce2cc60f142007
rishitha <rishithasiva1321@gmail.com>
2024-09-06 21:00:03 -0500

    sign in nd out

--- public/app.js
+++ public/app.js
@@ -1,10 +1,10 @@
-import { onClickHomeMenu,onClickMenu2Menu } from "./controller/menueventhandlers.js";
+import { onClickHomeMenu,onClickMenu2Menu, onClickSignOutMenu } from "./controller/menueventhandlers.js";
 import { signinPageView } from "./view/signin_page.js";
 import { attachAuthStateChangeObserver } from "./controller/firebase_auth.js";
 //menu button handler
 document.getElementById('menu-home').onclick = onClickHomeMenu;
 document.getElementById('menu-menu2').onclick = onClickMenu2Menu;
-
+document.getElementById('menu-signout').onclick = onClickSignOutMenu;
 attachAuthStateChangeObserver();
 
 signinPageView();
--- public/controller/menueventhandlers.js
+++ public/controller/menueventhandlers.js
@@ -1,9 +1,14 @@
 import { homePageView } from "../view/home_page.js";
 import { Menu2PageView } from "../view/menu2_page.js";
+import { signOutFirebase } from "./firebase_auth.js";
 
 export function onClickHomeMenu(e){
     homePageView();
 }
 export function onClickMenu2Menu(e){
     Menu2PageView();
+}
+
+export async function onClickSignOutMenu(e){
+    await signOutFirebase(e);
 }
