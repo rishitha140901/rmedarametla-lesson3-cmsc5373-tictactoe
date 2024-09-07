8a6e23bdf8a088cfb210ff3bef18a39dad997e07
rishitha <rishithasiva1321@gmail.com>
2024-09-06 20:35:22 -0500

    auth done

--- public/controller/firebase_auth.js
+++ public/controller/firebase_auth.js
@@ -2,11 +2,22 @@ import {
     getAuth, signInWithEmailAndPassword 
 } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-auth.js";
 import { app } from "./firebase_core.js";
+import { DEV } from "../model/constants.js";
 
 const auth = getAuth(app);
 
-export function signinFirebase(e) {
+export async function signinFirebase(e) {
     e.preventDefault();
     const email = e.target.email.value;
     const password = e.target.password.value;
+
+    try {
+        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+        const user = userCredential.user;
+    } catch (error) {
+        if (DEV) console.log('signin error:',error);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert('signin error:' + errorCode + ' ' + errorMessage);
+    }
 }
