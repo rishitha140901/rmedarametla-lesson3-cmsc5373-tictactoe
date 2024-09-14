7c58a1849a1d0a68508d6129253dd6f2865490ca
rishitha <rishithasiva1321@gmail.com>
2024-09-13 20:08:05 -0500

    game

--- public/controller/home_controller.js
+++ public/controller/home_controller.js
@@ -1,14 +1,23 @@
-export function onSubmitCalcForm(e){
-    e.preventDefault();
-    const nStr = e.target.number.value;
-    const n = parseInt(nStr);
-    const ul = document.getElementById('display');
-    const eqString= `
-    ${n} x ${n} = ${n*n}
+import { GameState } from "../model/tictactoe_game.js";
+import { game, updateWindow } from "../view/home_page.js";
+export function onclickBoardButton(e){
     
-    `;
-    const li= document.createElement('li');
-    li.textContent=eqString;
-    ul.appendChild(li);
-    e.target.number.value=''; //clear input
+    const pos= parseInt(e.currentTarget.value[3])
+    game.play(pos);
+    game.setWinner();
+    if (game.winner!=null){
+        game.gameState=GameState.DONE;
+    } else{
+        game.changeTurn();
+    }
+    updateWindow();
+
+
+}
+
+export function onclickNewGame(e){
+   
+    game.reset();
+    game.gameState = GameState.PLAYING;
+    updateWindow();
 }
--- public/view/home_page.js
+++ public/view/home_page.js
@@ -1,7 +1,15 @@
 import { currentUser } from "../controller/firebase_auth.js";
 import { root } from "./elements.js";
 import { protectedView } from "./protected_view.js";
-import { onSubmitCalcForm } from "../controller/home_controller.js";
+import { onclickBoardButton, onclickNewGame,  } from "../controller/home_controller.js";
+import { GameState, TicTacToeGame , marking} from "../model/tictactoe_game.js";
+
+export const images ={
+    X: '/images/X.png',
+    O: '/images/O.png',
+    U: '/images/XO.jpg',
+}
+export let game = new TicTacToeGame();
 
 export async function homePageView(){
     if (!currentUser){
@@ -15,7 +23,65 @@ export async function homePageView(){
     divWrapper.innerHTML=await response.text();
     divWrapper.classList.add('m-4','p-4')
 
+    const buttons = divWrapper.querySelectorAll('table button');
+    buttons.forEach(b=> b.onclick = onclickBoardButton);
+    const newGameButton = divWrapper.querySelector('#button-new-game');
+    newGameButton.onclick = onclickNewGame;
+
     
     root.innerHTML='';
     root.appendChild(divWrapper);
+
+    updateWindow();
 }
+
+
+ export function updateWindow(){
+
+    const buttons = document.querySelectorAll('table button');
+    const newGameButton=document.querySelector('#button-new-game');
+    const turnImg = document.querySelector('#turn');
+    turnImg.src = images[game.turn];
+
+    switch(game.gameState){
+        case GameState.INIT:
+            buttons.forEach(b=>b.disabled=true);
+            newGameButton.disable=false;
+            for(let i=0;i< game.board.length; i++){
+                const img =buttons[i].firstElementChild;
+                img.src = images[game.board[i]];
+            }
+            document.getElementById('message').innerHTML='Press New Game To Start';
+            break;
+        case GameState.PLAYING:
+            document.getElementById('message').innerHTML = `
+                   Click on the board to move.<br>
+                   (# of moves = ${game.moves})
+            `;
+            newGameButton.disabled=true;
+            for(let i=0;i<game.board.length;i++){
+                const img= buttons[i].firstElementChild;
+                img.src = images[game.board[i]];
+                buttons[i].disabled=game.board[i] !=marking.U;
+            }
+            break;
+        case GameState.DONE:
+            buttons.forEach(b=> b.disabled =true);
+            newGameButton.disabled=false;
+            for(let i=0;i<game.board.length;i++){
+                const img = buttons[i].firstElementChild;
+                img.src=images[game.board[i]];
+
+            }
+            let winner = game.winner + ' has won.';
+            if(game.winner == marking.U){
+                winner='Draw!';
+            }
+            let message=`
+            Game Over! ${winner}<br>
+            <br>press "New Game" to play again
+            `;
+            document.getElementById('message').innerHTML=message;
+            break;
+    }
+ }
